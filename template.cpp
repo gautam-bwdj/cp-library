227598c8123b0d173c37e2b3d2bede4d45e92111
gautam-bwdj <gautam.bwdj@gmail.com>
2026-02-15 03:14:53 +0530

    feat: add C++ competitive programming template

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define int long long
+#define endl "\n"
+
+constexpr int INF = 1e18;
+constexpr int MOD = 1e9 + 7;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+void solve() {
+    
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
